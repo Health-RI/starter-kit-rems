2bf221f8b29fca6fba4009494f3cb5ae2f331339
Stefan Negru <stefan@blankdots.com>
2023-02-22 11:16:16 +0000

    use authlib updated way of generating keys
    Authlib deprecated jwk.dumps and we should use the new way

--- generate_jwks.py
+++ generate_jwks.py
@@ -1,31 +1,15 @@
+from authlib.jose import RSAKey
 from json import dumps
-from authlib.jose import jwk
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
 
-public_key_file = "public-key.json"
-private_key_file = "private-key.json"
+public_key_file = "public-key.jwk"
+private_key_file = "private-key.jwk"
 
 if __name__ == "__main__":
 
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend(),
-    )
-    public_key = private_key.public_key().public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.NoEncryption(),
-    )
+    key = RSAKey.generate_key(is_private=True)
 
-    public_jwk = jwk.dumps(public_key, kty="RSA")
-    private_jwk = jwk.dumps(pem, kty="RSA")
+    public_jwk = key.as_dict(is_private=False)
+    private_jwk = dict(key)
 
     with open(public_key_file, "w") as f:
         f.write(dumps(public_jwk, indent=4))
